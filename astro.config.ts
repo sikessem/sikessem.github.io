9180ab4d25884f76d5e4e2e09625ec1ff3918611
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-08-06 12:53:52 +0100

    :lipstick: Add favicon

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
+			favicon: '/favicon.ico',
 			logo: {
 				src: './src/assets//logo.svg',
 				alt: 'Sikessem',
