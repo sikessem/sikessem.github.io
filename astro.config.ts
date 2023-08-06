463688ae3384298f1f1c6c5097326a4b4c6ea26b
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-08-06 18:29:30 +0100

    :heavy_plus_sign: Add Alpine.js

--- astro.config.ts
+++ astro.config.ts
@@ -1,47 +1,51 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    favicon: '/favicon.ico',
-    logo: {
-      src: './src/assets//logo.svg',
-      alt: 'Sikessem',
-      replacesTitle: true
-    },
-    title: 'Sikessem',
-    social: {
-      github: 'https://github.com/sikessem',
-      twitter: 'https://twitter.com/@sikessem_tweets'
-    },
-    locales: {
-      root: {
-        label: 'English',
-        lang: 'en'
+  integrations: [
+    starlight({
+      favicon: '/favicon.ico',
+      logo: {
+        src: './src/assets//logo.svg',
+        alt: 'Sikessem',
+        replacesTitle: true
       },
-      fr: {
-        label: 'Français'
-      }
-    },
-    sidebar: [{
-      label: 'Projects',
-      autogenerate: {
-        directory: 'projects'
-      }
-    }, {
-      label: 'Packages',
-      autogenerate: {
-        directory: 'packages'
-      }
-    }]
-  }),
-  tailwind({
-    configFile: './tailwind.config.ts',
-    applyBaseStyles: false,
-  })],
+      title: 'Sikessem',
+      social: {
+        github: 'https://github.com/sikessem',
+        twitter: 'https://twitter.com/@sikessem_tweets'
+      },
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en'
+        },
+        fr: {
+          label: 'Français'
+        }
+      },
+      sidebar: [{
+        label: 'Projects',
+        autogenerate: {
+          directory: 'projects'
+        }
+      }, {
+        label: 'Packages',
+        autogenerate: {
+          directory: 'packages'
+        }
+      }]
+    }),
+    tailwind({
+      configFile: './tailwind.config.ts',
+      applyBaseStyles: false
+    }),
+    alpinejs()
+  ],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
